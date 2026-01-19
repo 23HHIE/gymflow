b271c9be8d69b419eae834eb5ca85a627d4c30a8
trapper <129381435+23HHIE@users.noreply.github.com>
2026-01-19 12:47:32 +0000

    chore: bootstrap spring boot application

---
+++ src/main/java/com/gymflow/gymflow_app/GymflowAppApplication.java
@@ -0,0 +1,13 @@
+package com.gymflow.gymflow_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymflowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymflowAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymflow/gymflow_app/GymflowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymflow.gymflow_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymflowAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
